b781ebf9b69ef0b8e180ae451e1ca65fc8d5d436
oleoslemos <leoslemos@gmail.com>
2025-09-05 14:32:43 -0300

    Chore: atualizar variaveis e security_env para usar env; manter ajustes ETL

--- scr/utils/variaveis.py
+++ scr/utils/variaveis.py
@@ -11,3 +11,11 @@ clint_password = os.getenv("CLINT_PASSWORD")
 clint_urls = [
     "https://app.clint.digital/origin/1bb864dd-9fdf-498c-aaee-256776337fe8",
 ]
+
+pbi_tenant_id = os.getenv('PBI_TENANT_ID')
+pbi_client_id = os.getenv('PBI_CLIENT_ID')
+pbi_client_secret = os.getenv('PBI_CLIENT_SECRET')
+pbi_authority = os.getenv('PBI_AUTHORITY')
+pbi_scope = os.getenv('PBI_SCOPE')
+pbi_group_id = os.getenv('PBI_GROUP_ID')
+pbi_dataset_id = os.getenv('PBI_DATASET_ID')
